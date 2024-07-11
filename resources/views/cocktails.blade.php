52102406b3e7145f648640f94ca6eebb44af6d49
StefanoAntonelli93 <ste.antonelli@outlook.it>
2024-07-11 17:35:53 +0200

    feat: add card to cocktails.blade.php

--- resources/views/cocktails.blade.php
+++ resources/views/cocktails.blade.php
@@ -1,10 +1,36 @@
 @extends('layouts.app')
-
 @section('main')
-{{-- CONTENUTO DEI COKTAILS --}}
-@foreach ( as )
-    
-@endforeach
+    <div class="container mt-5">
+        <div class="row">
+            @foreach ($cocktails as $cocktail)
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title fw-semibold">{{ $cocktail->Nome }}</h5>
+                            <div class="mb-2">
+                                <strong>Partenza:</strong> {{ $cocktail->Categoria }}
+                            </div>
+                            <div class="mb-2">
+                                <strong>Arrivo:</strong> {{ $cocktail->Ingredienti }}
+                            </div>
+                            <div class="mb-2">
+                                <strong>Data partenza:</strong>
+                                {{ $cocktail->Istruzioni }}
+                            </div>
+                            <div class="mb-2">
+                                <strong>Data arrivo:</strong>
+                                {{ $cocktail->Decorazione }}
+                            </div>
+                            <div class="mb-2">
+                                <strong>Orario partenza:</strong>
+                                {{ $cocktail->Origine }}
+                            </div>
 
+                            <a href="#" class="btn btn-success">Seleziona</a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
-
