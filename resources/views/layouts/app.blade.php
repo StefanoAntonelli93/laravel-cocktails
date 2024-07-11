919e077d90e06568c58afeb8d272431376848a58
AlanDAmario <vega93@live.it>
2024-07-11 16:43:49 +0200

    feat: add section

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,12 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Cocktails</title>
+    <title>laravel-boolando</title>
+    <!-- Includiamo gli assets con la direttiva vite-->
+    @vite('resources/js/app.js')
 </head>
+
 <body>
-    
+    <div class="container py-4">
+
+        @include('shared.header')
+
+        <main>
+            @yield('main')
+        </main>
+
+        @include('shared.footer')
+
+
+    </div>
+
 </body>
+
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>laravel-boolando</title>
-    <!-- Includiamo gli assets con la direttiva vite-->
-    @vite('resources/js/app.js')
-</head>
-
-<body>
-    <h1>Laravel Template</h1>
-    <img src="{{ Vite::asset('resources/img/logo.jpg') }}" alt="pic">
-</body>
-
-</html>
+@section('main')
+    {{-- CONTENUTO HOMEPAGE --}}
+@endsection
