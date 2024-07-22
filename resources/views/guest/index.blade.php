89a005b02765bde966a2fc2aa57d3b95983d2737
Nicola-Lippo <nicolalippo8991@gmail.com>
2024-07-22 17:09:29 +0200

    feat: add layout section + Fount

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -2,12 +2,13 @@
 
 @section('main')
     <div class="container mt-5">
+        <h1 class="text-center my-5"><strong></strong></h1>
         <div class="row">
             @foreach ($cocktails as $cocktail)
                 <div class="col-md-4 mb-4">
-                    <div class="card">
+                    <div class="card h-100">
                         <div class="card-body">
-                            <h5 class="card-title fw-semibold">{{ $cocktail->name }}</h5>
+                            <h2 class="card-title fw-semibold mt-2">{{ $cocktail->name }}</h2>
                             <div class="mb-2">
                                 <strong>Ingredienti:</strong> {{ $cocktail->ingredients }}
                             </div>
@@ -18,8 +19,11 @@
                                 <strong>Origine:</strong>
                                 {{ $cocktail->origin }}
                             </div>
-
-                            <a href="#" class="btn btn-success">Seleziona</a>
+                        </div>
+                        <div class="d-flex justify-content-around card-footer">
+                            <a href="#" class="btn btn-primary m-3"><i class="fa-solid fa-magnifying-glass"></i></a>
+                            <a href="#" class="btn btn-warning m-3"><i class="fa-solid fa-pen"></i></a>
+                            <a href="#" class="btn btn-danger m-3"><i class="fa-solid fa-bomb"></i></a>
                         </div>
                     </div>
                 </div>
