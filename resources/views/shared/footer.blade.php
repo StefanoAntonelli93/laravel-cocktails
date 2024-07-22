90b7d1de72dee3669019853369d2cb1d72ada6a0
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-07-22 16:54:31 +0200

    feat: added footer style

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -1,1 +1,32 @@
-<footer>Footer dei cocktails</footer>
+<footer class="bg-primary">
+    <div class="container py-3 mt-3">
+        <div class="row">
+            <div class="col-6">
+                <div>
+                    <h2 style="color:white">Bar Alan</h2>
+                    <a style="color:white" href="">Informazioni legali</a>
+                    <a style="color:white" href="">Informativa sulla privacy</a>
+                    <a style="color:white" href="">Bevi responsabilmente</a>
+                </div>
+            </div>
+            <div class="col-6 d-flex justify-content-end align-items-end">
+                <div>
+                    <h2 style="color: white">Trovaci anche su</h2>
+                    <div class="d-flex justify-content-end align-items-end">
+
+                        <img class="social-icon" src="{{ Vite::asset('resources/img/icons8-twitter.svg') }}"
+                            alt="twitter" />
+                        <img class="social-icon" src="{{ Vite::asset('resources/img/icons8-facebook.svg') }}"
+                            alt="facebook" />
+                        <img class="social-icon" src="{{ Vite::asset('resources/img/icons8-instagram.svg') }}"
+                            alt="instagram" />
+                        <img class="social-icon" src="{{ Vite::asset('resources/img/icons8-pinterest.svg') }}"
+                            alt="pinterest" />
+                        <img class="social-icon" src="{{ Vite::asset('resources/img/icons8-youtube.svg') }}"
+                            alt="youtube" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
